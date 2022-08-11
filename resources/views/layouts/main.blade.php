f5efa444b481a04499ec2771f0c6a322ca79f552
Marcilio128 <marciliogomes2005@gmail.com>
2022-08-11 15:39:33 -0300

    criação da section one na welcome

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,9 +3,18 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500&display=swap" rel="stylesheet">
+
         
         <link rel="stylesheet" href="./css/reset.css">
         <link rel="stylesheet" href="./css/static.css">
+        <link rel="stylesheet" href="./css/welcome.css">
+
+        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+
         <title>@yield('title')</title>
         
     </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,28 @@
 @section('title', '3E')
 
 @section('content')
+<section class="sec-welcomeOne">
+    <div class="container">
+        <div class="text-welcome">
+        <h1>Você conhece a importancia do descarte conciênte?</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="animation">
+                <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_mndgsj3k.json"  background="transparent"  speed="1"    loop  autoplay></lottie-player>
+            </div>
+        </div>
+</section>
 
+<section class="sec-welcometwo">
+    <div class="container">
+        <h1>Conheça sobre o descarte correto</h1>
+        <div>
+
+        </div>
+    </div>
+</section>
 @endsection
