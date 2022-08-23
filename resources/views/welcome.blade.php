ee3628d8c64514794b3639fa6ef1eb6f352da60d
Marcilio128 <marciliogomes2005@gmail.com>
2022-08-23 16:40:26 -0300

    texto para a welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,11 +8,12 @@
     <div class="container">
         <div class="text-welcome">
             <h1>Conheça a importancia do descarte consciente</h1>
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p> A sustentabilidade visa a preservação e manuntenção do meio ambiente, 
+                por diversas formas ou meios. E é de suma importância a sua presença no mundo 
+                para que seja possível manter a vida na Terra, para que futuramente nossas crianças 
+                tenham uma natureza para apreciar, um planeta para morar. 
+                A 3e Soluções também vem realizando ações sustentáveis para deixar o nosso planeta 
+                melhor no presente, para se manter no futuro.</p>
         </div>
         <lottie-player class="animation" src="https://assets9.lottiefiles.com/packages/lf20_mndgsj3k.json"  background="transparent"  speed="1"    loop  autoplay></lottie-player>
     </div>
