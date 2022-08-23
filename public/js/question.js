69fd53491528c1e98e0edc9939c38a4994678ccd
Marcilio128 <marciliogomes2005@gmail.com>
2022-08-23 16:37:44 -0300

    atualização das perguntas no quiz

--- public/js/question.js
+++ public/js/question.js
@@ -1,98 +1,204 @@
-const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-text'));
-const score = document.querySelector('#score');
-const progressText = document.querySelector('#progressText');
+let instrucoes = document.querySelector('#instrucoes')
+let aviso = document.querySelector('#aviso')
+let pontos = 0 // pontos para o placar
+let placar = 0 // placar
 
-let currentQuestion = {}
-let acceptAnserws = true 
-let points = 0
-let questionCounter = 0
-let AvaliableQuestions = []
+// PERGUNTA
+let numQuestao = document.querySelector('#numQuestao')
+let pergunta   = document.querySelector('#pergunta')
 
+// ALTERNATIVAS
+let a = document.querySelector('#a')
+let b = document.querySelector('#b')
+let c = document.querySelector('#c')
+let d = document.querySelector('#d')
 
-let questions = [
-    {
-        question: 'Qual o descarte correto a ser feito em lixeiras vermelhas e amarelas?',
-        choice1: 'Metal & Plastico',
-        choice2: 'Metal & Papel',
-        choice3: 'Papel & Vidros',
-        choice4: 'Organicos & Papeis',
-        answer: 1,
-    },
-    {
-        question: 'Qual o projeto responsável por troca de residuos em desconstos na conta de energia?',
-        choice1: 'Ecocelpa',
-        choice2: 'Residuos & Energias',
-        choice3: 'Ecoenel',
-        choice4: 'Enel more',
-        answer: 3,
+// article com a class questoes
+let articleQuestoes = document.querySelector('.questoes')
+// ol li com as alternativas
+let alternativas = document.querySelector('#alternativas')
 
-    },
-]
-
-const Score_Points = 1;
-const Max_Questions = 2;
-
-startGame = () => {
-    currentQuestion = 0;
-    points = 0
-    questionCounter = 0
-    AvaliableQuestions = [...questions]
-    getNewQuestion()
+const q0 = {
+    numQuestao   : 0,
+    pergunta     : "Pergunta",
+    alternativaA : "Alternativa A",
+    alternativaB : "Alternativa B",
+    alternativaC : "Alternativa C",
+    alternativaD : "Alternativa D",
+    correta      : "0",
 }
 
-getNewQuestion = () => {
-    if(AvaliableQuestions.length === 0 || questionCounter > Max_Questions) {
-        localStorage.setTimeout('mostRecentScore', score)
+const q1 = {
+    numQuestao   : 1,
+    pergunta     : "O descarte de plastico e metal devem ser feitos, em lixeiras de cores:",
+    alternativaA : "Azul & Verde",
+    alternativaB : "Vermelha & Amarela",
+    alternativaC : "Verde & Marrom",
+    alternativaD : "Branca e Laranja",
+    correta      : "Vermelha & Amarela",
 
-        return window.location.assing('/end.html')
-    }
-
-    questionCounter++
-    progressText.innerHTML = `Question ${questionCounter} of ${Max_Questions}` 
-    ProgressBarFull = `${(questionCounter/Max_Questions) * 100}%`
-    
-    const questionIndex = Math.floor(Math.random() * AvaliableQuestions.length)
-    currentQuestion = AvaliableQuestions[questionIndex]
-    question.innerHTML = currentQuestion.question
-
-    choices.forEach(choice => {
-        const number = choice.dataset['number']
-        choice.innerHTML = currentQuestion['choice' + number]
-    })
-
-    AvaliableQuestions.splice(questionIndex, 1)
-
-    acceptAnserws = true
 }
 
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptAnserws) return
+const q2 = {
+    numQuestao   : 2,
+    pergunta     : "O projeto responsável por trocar resíduos recicláveis por bônus na conta de luz: ",
+    alternativaA : "Ecocelpa",
+    alternativaB : "Enel +",
+    alternativaC : "Luz e vida",
+    alternativaD : "Ecoenel",
+    correta      : "Ecoenel",
+}
 
-        acceptAnserws = false 
-        const selectedchoice = e.target
-        const selectedAnswer = selectedchoice.dataset['number']
-        
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorret'
+const q3 = {
+    numQuestao   : 3,
+    pergunta     : "Qual a importância da sustentabilidade?",
+    alternativaA : "Conservar o meio ambiente para manter a Terra saudável e que futuras gerações tenham o melhor ambiente para morar",
+    alternativaB : "Preservar a natureza para que o homem possa usar para ações industriais futuras",
+    alternativaC : "Ajudar a manter as pessoas nas suas casas da cidade",
+    alternativaD : "Conservar o meio ambiente para ajudar a evitar única e exclusivamente o efeito estufa",
+    correta      : "Conservar o meio ambiente para manter a Terra saudável e que futuras gerações tenham o melhor ambiente para morar",
+}
 
-        if(classToApply === 'correct') {
-            incrementScore(Score_Points)
+const q4 = {
+    numQuestao   : 4,
+    pergunta     : "Quais das opções abaixo não é uma forma sustentável de gerar energia elétrica?",
+    alternativaA : "Eólica",
+    alternativaB : "Petróleo",
+    alternativaC : "Nuclear",
+    alternativaD : "Carvão mineral",
+    correta      : "Petróleo",
+}
+
+const q5 = {
+    numQuestao   : 5,
+    pergunta     : "Quais são os benefícios da sustentabilidade?",
+    alternativaA : "Evitar vários problemas ambientais que podem acarretar em grandes desastres no futuro",
+    alternativaB : "Evitar que o homem desmate árvores",
+    alternativaC : "Diminuir a obesidade",
+    alternativaD : "Causar peso na consciência nos praticantes de atos contra o meio ambiente",
+    correta      : "Evitar vários problemas ambientais que podem acarretar em grandes desastres no futuro",
+}
+
+// CONSTANTE COM UM ARRAY DE OBJETOS COM TODAS AS QUESTOES
+const questoes = [q0, q1, q2, q3, q4, q5]
+
+let numero = document.querySelector('#numero')
+let total  = document.querySelector('#total')
+
+numero.textContent = q1.numQuestao
+
+let totalDeQuestoes = (questoes.length)-1
+console.log("Total de questões " + totalDeQuestoes)
+total.textContent = totalDeQuestoes
+
+// MONTAR A 1a QUESTAO COMPLETA, para iniciar o Quiz
+numQuestao.textContent = q1.numQuestao
+pergunta.textContent   = q1.pergunta
+a.textContent = q1.alternativaA
+b.textContent = q1.alternativaB
+c.textContent = q1.alternativaC
+d.textContent = q1.alternativaD
+
+
+// CONFIGURAR O VALUE INICIAL DA 1a QUESTAO COMPLETA
+a.setAttribute('value', '1A')
+b.setAttribute('value', '1B')
+c.setAttribute('value', '1C')
+d.setAttribute('value', '1D')
+
+// PARA MONTAR AS PROXIMAS QUESTOES
+function proximaQuestao(nQuestao) {
+    numero.textContent = nQuestao
+    numQuestao.textContent = questoes[nQuestao].numQuestao
+    pergunta.textContent   = questoes[nQuestao].pergunta
+    a.textContent = questoes[nQuestao].alternativaA
+    b.textContent = questoes[nQuestao].alternativaB
+    c.textContent = questoes[nQuestao].alternativaC
+    d.textContent = questoes[nQuestao].alternativaD
+    a.setAttribute('value', nQuestao+'A')
+    b.setAttribute('value', nQuestao+'B')
+    c.setAttribute('value', nQuestao+'C')
+    d.setAttribute('value', nQuestao+'D')
+
+}
+
+function bloquearAlternativas() {
+    a.classList.add('bloqueado')
+    b.classList.add('bloqueado')
+    c.classList.add('bloqueado')
+    d.classList.add('bloqueado')
+
+}
+
+function desbloquearAlternativas() {
+    a.classList.remove('bloqueado')
+    b.classList.remove('bloqueado')
+    c.classList.remove('bloqueado')
+    d.classList.remove('bloqueado')
+
+}
+
+function verificarSeAcertou(nQuestao, resposta) {
+
+    let numeroDaQuestao = nQuestao.value
+    console.log("Questão " + numeroDaQuestao)
+
+    let respostaEscolhida = resposta.textContent
+
+    let certa = questoes[numeroDaQuestao].correta
+
+    if(respostaEscolhida == certa) {
+        pontos += 10
+    } 
+    else {}
+
+    // atualizar placar
+    placar = pontos
+    instrucoes.textContent = "Pontos " + placar
+
+    // bloquear a escolha de opcoes
+    bloquearAlternativas()
+
+    setTimeout(function() {
+        //respostaEsta.textContent = '...'
+        proxima = numeroDaQuestao+1
+
+        if(proxima > totalDeQuestoes) {
+            console.log('Fim do Jogo!')
+            fimDoJogo()
+        } else {
+            proximaQuestao(proxima)
         }
-        selectedchoice.parentElement.classList.add(classToApply)
-
-        setTimeout(() => {
-            selectedchoice.parentElement.remove(classToApply)
-            getNewQuestion()
-
-        }, 1000)
-    })
-})
-
-incrementScore = num => {
-    score +=num
-    scoreText.innerHTML = score
+    }, 250)
+    desbloquearAlternativas()
 }
 
-startGame()
+function fimDoJogo() {
+    instrucoes.textContent = "Fim de Jogo!"
+    numQuestao.textContent = ""
 
+    let pont = ''
+    pontos == 0 ? pont = 'ponto' : pont = 'pontos'
+
+    pergunta.textContent   = "Você conseguiu " + pontos + " " + pont
+
+    aviso.textContent = "Você conseguiu " + pontos + " " + pont
+
+    a.textContent = ""
+    b.textContent = ""
+    c.textContent = ""
+    d.textContent = ""
+
+    a.setAttribute('value', '0')
+    b.setAttribute('value', '0')
+    c.setAttribute('value', '0')
+    d.setAttribute('value', '0')
+
+    // OCULTAR O ARTICLE DA QUESTAO
+    articleQuestoes.style.display = 'none'
+
+    setTimeout(function() {
+        pontos = 0 // zerar placar
+        location.reload();
+    }, 2000)
+}
--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -8,60 +8,48 @@
     <link rel="stylesheet" href="./css/question.css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Questions</title>
 </head>
 <body>
-    <section class="questions">
-        <div class="container">
-            <div id="game">
-                <div id="hud" class="itens">
-                    <div class="hud-item">
-                        <img src="/assets/3e-logo-color.png" alt="" srcset="">
-                        <p id="progressText" class="hub-prefix">
-                            Question
-                        </p>
-                        <div id="progressBar">
-                            <div id="progressbarFull"></div>
-                        </div>
-                    </div>
-                    <div class="hub-item">
-                    <div id="question">Pergunta </div>
+<main class="tela-principal">
+<svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="rgb(203,86,22)" fill-opacity="1" d="M0,224L60,240C120,256,240,288,360,256C480,224,600,128,720,106.7C840,85,960,139,1080,149.3C1200,160,1320,128,1380,112L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+</svg>
+    <section class='conteudos'>
+        
+        
+        <article class='questoes'>
+            
+            <header class='questao'>
+            <img class="Logo" src="./assets/3e-logo-color.png" alt="" srcset="">
+            <article id='aviso' class="questions" >Questão <span id='numero'></span>/<span id='total'></span></article>
+            <span id='numQuestao'></span>
+            <h2 id='pergunta'></h2>
+        </header>
+        
+        <div class = "vertical"></div>
 
-                    </div>
-                </div>
-                <div>
-                    </div>
-                    
-                    <div class = "vertical"></div>
-                    
-                    
-                    <div id="score">
-                        <p>
-                            Score
-                        </p>
-                        <h1 class="hub-main-text" id="score">
-                            0/10
-                        </h1>
-                        <div class="choice-container">
-                        <button class="choice-text" data-number="1">Escolha </button>
-                        
-                    </div>
-                    <div class="choice-container">
-                        <button class="choice-text" data-number="2">Escolha 2 </button>
-                    </div>
+        <div class='choices'>
+        <article class='points' id='instrucoes'>
+        Pontos 0
+    </article>
+            <ol type='A' id='alternativas'>
+                <li id='a' value='1A' class='respostas' onClick='verificarSeAcertou(this, this)'></li>
+                <li id='b' value='1B' class='respostas' onClick='verificarSeAcertou(this, this)'></li>
+                <li id='c' value='1C' class='respostas' onClick='verificarSeAcertou(this, this)'></li>
+                <li id='d' value='1D' class='respostas' onClick='verificarSeAcertou(this, this)'></li>
 
-                    <div class="choice-container">
-                        <button class="choice-text" data-number="3">Escolha 3</button>
-                    </div>
-
-                    <div class="choice-container">
-                        <button class="choice-text" data-number="4">Escolha 4</button>
-                    </div>
-                </div>
-            </div>
-
+            </ol>
         </div>
-    </section>
+    </article>
+    
+    <svg class="wave2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="#ffd700" fill-opacity="1" d="M0,256L34.3,229.3C68.6,203,137,149,206,154.7C274.3,160,343,224,411,213.3C480,203,549,117,617,96C685.7,75,754,117,823,160C891.4,203,960,245,1029,218.7C1097.1,192,1166,96,1234,74.7C1302.9,53,1371,107,1406,133.3L1440,160L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+</svg>
+</section>
+
+</main>
 
 </body>
 <script src="./js/question.js"></script>
