8d836e82d675dfa63e466f4d399fa2bfc9c331dd
Marcilio128 <marciliogomes2005@gmail.com>
2022-09-14 14:23:56 -0300

    fav-icon na tela de registro

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -8,9 +8,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/reset.css">
-
     <link rel="stylesheet" href="./css/registerQuiz.css">
-
+    <link rel="icon"  href="./assets/3e-icon-color.ico">
 
     <title>Registro</title>
 </head>
