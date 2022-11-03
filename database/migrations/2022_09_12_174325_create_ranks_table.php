8edaf62a6b02266ccd398d543e127413738bf2bf
DavidMes17 <davidmes46@gmail.com>
2022-11-03 14:25:29 -0300

    remoção unique

--- database/migrations/2022_09_12_174325_create_ranks_table.php
+++ database/migrations/2022_09_12_174325_create_ranks_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('ranks', function (Blueprint $table) {
             $table->id();
-            $table->string('nome',255)->nullable()->unique();
+            $table->string('nome',255)->nullable();
             $table->integer('pontos')->nullable();
             $table->time('tempo')->nullable();
             $table->timestamps();
